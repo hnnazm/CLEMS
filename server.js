30fc28a04c7129adade51357fd62860fa81d9e0b
Hannan Azmi <mhannan143@gmail.com>
2021-03-16 17:17:42 +0800

    Use ejs to render page

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const config = require('./config')
+// let ejs = require('ejs')
 const express = require('express')
 const middleware = require('./middleware')
 const path = require('path')
@@ -6,14 +7,17 @@ const session = require('express-session')
 
 const app = express()
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
 app.use(express.urlencoded({ extended: true }))
 app.use(session(config.SESSION_OPTION));
 app.use(express.static(path.join(__dirname, 'dist'), {index: false}))
 
 app.get('/', middleware.checkAuthentication, (req, res) => {
-    // res.send("<h1>Home Page</h1>")
-    res.send({isAuthenticate: true})
-    res.send({isAuthenticate: 'asd'})
+    res.render('index', {
+        title: 'HOME'
+    })
 })
 
 app.route('/login')
@@ -21,6 +25,9 @@ app.route('/login')
         // res.sendFile(path.join(__dirname, 'public','login.html'))
         res.sendFile(path.join(__dirname, 'dist', 'index.html'))
         // res.send("<h1>Login Page</h1>")
+        res.render('login', {
+            title: 'LOGIN'
+        })
     })
     .post(function (req, res) {
         if (req.body.username === 'admin' && req.body.pwd === 'secret') {
