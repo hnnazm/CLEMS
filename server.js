3bd451d72d9430e0acf1a8ab180988891be2f694
Hannan Azmi <mhannan143@gmail.com>
2021-03-24 17:14:19 +0800

    Setup MongoDB

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 const config = require('./config')
 const http = require('http')
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
 const route = require('./route')
 const session = require('express-session')
 const socketIO = require('socket.io')
-const socketHandler = require('./socket')
 
 const app = express()
 const server = http.createServer(app)
-const io = socketIO(server, config.SOCKET_OPTION)
+const io = socketIO(server, config.SOCKET_OPTIONS)
+const db = mongoose.connection
 
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.resolve(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.use(express.urlencoded({ extended: true }))
-app.use(session(config.SESSION_OPTION))
-app.use(express.static(path.join(__dirname, 'public'), {index: false}))
+app.use(express.urlencoded(config.EXPRESS_URLENCODED_OPTIONS))
+app.use(session(config.SESSION_OPTIONS))
+app.use(express.static(path.resolve(__dirname, 'public'), {index: false}))
 app.use('/', route)
+
+mongoose.connect(config.MONGOOSE_URI, config.MONGOOSE_OPTIONS)
+db.on('error', () => console.error('Error connecting to database:'))
+db.once('open', () => console.log('Database connected!'))
 
 io.on("connection", socket => {
     console.log("New client connected")
@@ -34,6 +39,6 @@ io.on("connection", socket => {
     })
 });
 
-server.listen(config.PORT, config.HOST, () => {
+server.listen(config.PORT, () => {
     console.log(`Server is running on port ${config.PORT}`)
 })
