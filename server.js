31b911d48a7225ba73a3dd22442b641f46024f23
Hannan Azmi <mhannan143@gmail.com>
2021-04-19 07:01:06 +0800

    Revert to unmodular io

--- server.js
+++ server.js
@@ -23,8 +23,54 @@ app.use(express.static(path.resolve(__dirname, 'public'), {index: false}))
 app.use(route)
 
 mongoose.connect(config.MONGOOSE_URI, config.MONGOOSE_OPTIONS)
-db.on('error', () => console.error('Error connecting to database:'))
-db.once('open', () => console.log('Database connected!'))
+mongoose.set('useFindAndModify', false)
+//LOG: db.on('error', () => console.error('Error connecting to database:'))
+//LOG: db.once('open', () => console.log('Database connected!'))
+
+io.use((socket, next) => {sessionMiddleware(socket.request, {}, next)})
+
+io.on("connection", async socket => {
+    // session management
+    const session = socket.request.session
+    session.connections++;
+    session.save();
+
+    const ip = socket.conn.remoteAddress.split(":").pop();
+
+
+    //LOG: console.log("New client connected")
+    await User.findOneAndUpdate({ address: socket.request.sessionID }, {
+        // insert socket id to user document
+        socketID: socket.id
+    }, { new: true })
+
+    // Incoming new message
+    socket.on('newMessage', async message => {
+        // const user = await User.findOne({socketID: socket.id})
+        User.findOne({socketID: socket.id}, (err, user) => {
+            //LOG: if (err) console.error(err)
+            Data.create({
+                type: typeof message,
+                data: message,
+                sender: user.id,
+                receiver: 'to you',     // TODO: change to user/room
+                size: 12345             // TODO: insert correct size
+            }, (err, data) => {
+                //LOG: if (err) console.error(err)
+                //LOG: console.log(data)
+                io.emit('receiveMessage', message, {
+                    id: user.socketID,
+                    username: user.username
+                })
+            })
+        })
+    })
+
+    // Client disconnect
+    socket.on('disconnect', () => {
+        //LOG: console.log("Client disconnect")
+    })
+});
 
 server.listen(config.PORT, () => {
     console.log(`Server is running on port ${config.PORT}`)
