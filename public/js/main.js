0174c729e4c968c14dc0cf68219d7c694c8bdb95
Hannan Azmi <mhannan143@gmail.com>
2021-04-19 06:58:53 +0800

    Update UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,30 @@
 const clientSocket = io();
 
+// Handle the browser frame that affect viewport
+(function viewport_adjustment() {
+  let vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+})()
+window.addEventListener('resize', () => viewport_adjustment());
+
 // Elements selector
 const chat = document.querySelector('#chat-content')
 const form = document.querySelector('#chat-input > form')
+const logoutButton = document.querySelector('#btn-logout')
+const settingsButton = document.querySelector('#btn-settings')
 
 // Client join room
 clientSocket.emit('join')
 
 // Confirm message sent and output to DOM
-clientSocket.on('receiveMessage', (data, id) => {
-    console.log(data)
-    outputMessage(data, id)
+clientSocket.on('receiveMessage', (data, user) => {
+    outputMessage(data, user)
 
     chat.scrollTop = chat.scrollHeight
 })
+
+logoutButton.addEventListener('click', () => window.location.href='/logout')
+settingsButton.addEventListener('click', () => window.location.href='/settings')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -22,7 +33,6 @@ form.addEventListener('submit', (e) => {
     let data = e.target.elements.message.value;
 
     data = data.trim();
-    console.log(data)
 
     if (!data) {
         return false;
@@ -37,21 +47,28 @@ form.addEventListener('submit', (e) => {
 });
 
 // Output message to DOM
-function outputMessage(data, id) {
-    let time = new Date()
+function outputMessage(data, user) {
+    const time = new myTime()
     const container = document.createElement('div')
     const sender = document.createElement('p')
     const message = document.createElement('p')
-
-    time = (time.getHours() < 10 ? "0" : "")  + time.getHours() + ":" + (time.getMinutes() < 10 ? "0" : "") + time.getMinutes()
     
-    container.classList.add(id === clientSocket.id ? 'message-to' : 'message-from')
+    container.classList.add(user.id === clientSocket.id ? 'message-to' : 'message-from')
     message.classList.add('message-content')
 
-    sender.innerText = `${id} | ${time}`
+    sender.innerText = `${user.username} | ${time.current}`
     message.innerText = data
 
     container.appendChild(sender)
     container.appendChild(message)
     chat.appendChild(container)
+}
+
+function myTime() {
+    this.raw = new Date()
+    this.hours = (this.raw.getHours() < 10 ? "0" : "")  + this.raw.getHours()
+    this.minutes = (this.raw.getMinutes() < 10 ? "0" : "")  + this.raw.getMinutes()
+    this.current = `${this.hours}:${this.minutes}`
+
+    return this
 }
