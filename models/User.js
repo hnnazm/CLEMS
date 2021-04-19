f81e9138588c81609d6ddf28cf6a1d4ec02dd4a7
Hannan Azmi <mhannan143@gmail.com>
2021-04-19 08:59:02 +0800

    Update formatting

--- models/User.js
+++ models/User.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    address: {
-        type: String,
-        default: '',
-    },
-    username: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        default: 'members'
-    },
-    socketID: {
-        type: String,
-        default: ''
-    },
-    options: {
-        // nested schematype
-        mutedUser: {
-            type: [String],     // array of username
-            default: [],
-        }
+  address: {
+    type: String,
+    default: '',
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: 'members'
+  },
+  socketID: {
+    type: String,
+    default: ''
+  },
+  options: {
+    // nested schematype
+    mutedUser: {
+      type: [String],     // array of username
+      default: [],
     }
+  }
 })
 
 module.exports = mongoose.model('User', userSchema)
