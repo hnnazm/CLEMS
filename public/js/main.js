ecc810962e3c1c0112fbb4a16f1cf16e1e3903bc
Hannan Azmi <mhannan143@gmail.com>
2021-04-25 06:03:50 +0800

    Update formatting

--- public/js/main.js
+++ public/js/main.js
@@ -18,57 +18,57 @@ clientSocket.emit('join')
 
 // Confirm message sent and output to DOM
 clientSocket.on('receiveMessage', (data, user) => {
-    outputMessage(data, user)
+  outputMessage(data, user)
 
-    chat.scrollTop = chat.scrollHeight
+  chat.scrollTop = chat.scrollHeight
 })
 
 logoutButton.addEventListener('click', () => window.location.href='/logout')
 settingsButton.addEventListener('click', () => window.location.href='/settings')
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    // Get message text
-    let data = e.target.elements.message.value;
+  // Get message text
+  let data = e.target.elements.message.value;
 
-    data = data.trim();
+  data = data.trim();
 
-    if (!data) {
-        return false;
-    }
+  if (!data) {
+    return false;
+  }
 
-    // Emit message to server
-    clientSocket.emit('newMessage', data);
+  // Emit message to server
+  clientSocket.emit('newMessage', data);
 
-    // Clear input
-    e.target.elements.message.value = '';
-    e.target.elements.message.focus();
+  // Clear input
+  e.target.elements.message.value = '';
+  e.target.elements.message.focus();
 });
 
 // Output message to DOM
 function outputMessage(data, user) {
-    const time = new myTime()
-    const container = document.createElement('div')
-    const sender = document.createElement('p')
-    const message = document.createElement('p')
-    
-    container.classList.add(user.id === clientSocket.id ? 'message-to' : 'message-from')
-    message.classList.add('message-content')
+  const time = new myTime()
+  const container = document.createElement('div')
+  const sender = document.createElement('p')
+  const message = document.createElement('p')
 
-    sender.innerText = `${user.username} | ${time.current}`
-    message.innerText = data
+  container.classList.add(user.id === clientSocket.id ? 'message-to' : 'message-from')
+  message.classList.add('message-content')
 
-    container.appendChild(sender)
-    container.appendChild(message)
-    chat.appendChild(container)
+  sender.innerText = `${user.username} | ${time.current}`
+  message.innerText = data
+
+  container.appendChild(sender)
+  container.appendChild(message)
+  chat.appendChild(container)
 }
 
 function myTime() {
-    this.raw = new Date()
-    this.hours = (this.raw.getHours() < 10 ? "0" : "")  + this.raw.getHours()
-    this.minutes = (this.raw.getMinutes() < 10 ? "0" : "")  + this.raw.getMinutes()
-    this.current = `${this.hours}:${this.minutes}`
+  this.raw = new Date()
+  this.hours = (this.raw.getHours() < 10 ? "0" : "")  + this.raw.getHours()
+  this.minutes = (this.raw.getMinutes() < 10 ? "0" : "")  + this.raw.getMinutes()
+  this.current = `${this.hours}:${this.minutes}`
 
-    return this
+  return this
 }
