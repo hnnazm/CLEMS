bb087c3b9a1b7c0c31abebe3c41561139a60e2f6
Hannan Azmi <mhannan143@gmail.com>
2021-05-09 10:16:59 +0800

    Update formatting

--- middleware.js
+++ middleware.js
@@ -1,13 +1,13 @@
 // middleware function
 
 function checkAuthentication(req, res, next) {
-    if (req.session.isAuthenticate) {
-        next()
-    } else {
-        res.redirect('/login')
-    }
+  if (req.session.isAuthenticate) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
 }
 
 module.exports = {
-    checkAuthentication
+  checkAuthentication,
 }
