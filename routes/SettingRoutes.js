cc5badc7d485f5b6d0196e82cf4f416b95952289
Hannan Azmi <mhannan143@gmail.com>
2021-05-20 15:18:05 +0800

    Add new functionality to drop database, reboot and shutdown device

--- routes/SettingRoutes.js
+++ routes/SettingRoutes.js
@@ -1,3 +1,5 @@
+const middleware = require('../middleware')
+const mongoose = require('mongoose')
 const router = require('express').Router()
 const User = require('../models/User')
 
@@ -29,6 +31,27 @@ router.post('/roomname', (req, res) => {
     }
     else res.send({ err: "Failed to change room name", roomname: null })
   } else res.status(403).end()
+})
+
+router.get('/dropdb', middleware.checkAuthentication, (req, res) => {
+  res.send('Hello')
+  if (req.session.isAdmin) {
+    mongoose.connection.db.dropDatabase();
+  }
+})
+
+router.get('/reboot', middleware.checkAuthentication, (req, res) => {
+  if (req.session.isAdmin) {
+    process.kill(process.pid, 'SIGTERM')
+    require('child_process').exec('reboot')
+  }
+})
+
+router.get('/terminate', middleware.checkAuthentication, (req, res) => {
+  if (req.session.isAdmin) {
+    process.kill(process.pid, 'SIGTERM')
+    require('child_process').exec('shutdown')
+  }
 })
 
 module.exports = router
